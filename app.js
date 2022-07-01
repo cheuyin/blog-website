962d7ca8e07b28e5bd392c8a552973b286b228b3
Yin <yinstanleycheung@gmail.com>
2022-07-01 12:18:32 -0700

    Connect rendering of home page to / route

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.get("/", (req, res) => {
+  res.render("home");
+})
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
